0effa579b00f14986e9921b1f293e46b8e9c9110
Justin <justin-urbanek@bluewin.ch>
2024-03-06 11:36:09 +0100

    feat: prototype qr-code scanning

--- src/app/qr-code/qr-code.page.ts
+++ src/app/qr-code/qr-code.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { AlertService } from 'src/services/AlertService';
 
 @Component({
   selector: 'app-qr-code',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./qr-code.page.scss'],
 })
 export class QrCodePage implements OnInit {
+  isSupported = false;
+  barcodes: Barcode[] = [];
 
-  constructor() { }
+  constructor(private alertService: AlertService) { }
 
   ngOnInit() {
+    BarcodeScanner.isSupported().then((res) => {
+      this.isSupported = res.supported;
+    });
+  }
+
+  async scan(): Promise<void> {
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.alertService.PresentAlert('Permission denied', ['OK'], 'Please grant camera permission to use the barcode scanner.');
+      return;
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
+  }
+
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
   }
 
   scanCode() {
