5c263d6c7738e24551d3a65e134c728bf4aad79d
Justin <justin-urbanek@bluewin.ch>
2024-03-06 14:34:09 +0100

    feat: tabSettings save and load user from storage

--- src/app/tabSettings/tabSettings.page.ts
+++ src/app/tabSettings/tabSettings.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from "src/services/user.service";
+import { User } from 'src/models/User';
 
 @Component({
   selector: 'app-tabSettings',
@@ -6,7 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['tabSettings.page.scss']
 })
 export class TabSettingsPage {
+  user: User;
 
-  constructor() { }
+  constructor(private userService: UserService) {
+    this.user = new User('', '');
+  }
 
+  async ngOnInit() {
+    await this.loadUser()
+  }
+
+  async saveUser() {
+    await this.userService.setUser(this.user);
+  }
+
+  async loadUser() {
+    this.user = await this.userService.getUser();
+  }
+
+  async resetUser() {
+    this.user = new User('', '');
+    await this.userService.setUser(this.user);
+  }
 }
