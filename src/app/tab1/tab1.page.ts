4cd9b0035f2fceb157d4f93c38e698a19e48bdbf
Justin <justin-urbanek@bluewin.ch>
2024-03-06 08:49:07 +0100

    refactor: rename Helper dictonary to lowercase

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ScavengerHunt } from 'src/models/ScavengerHunt';
 import { User } from 'src/models/User';
-import { UserHelper } from 'src/Helper/UserHelper';
+import { UserHelper } from 'src/helper/UserHelper';
 import { AlertService } from 'src/Services/AlertService';
 import { SCAVENGERHUNTS } from 'src/mocks/ScavengerHuntMock';
 
