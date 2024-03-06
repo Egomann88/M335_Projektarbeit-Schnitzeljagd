7d47bee775aef4e30950597c72900d84f65d1691
Justin <justin-urbanek@bluewin.ch>
2024-03-06 08:38:05 +0100

    feat: home tab import mock data and seperate hunt views

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ScavengerHunt } from 'src/models/ScavengerHunt';
+import { SCAVENGERHUNTS } from 'src/mocks/ScavengerHuntMock';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  scavengerHunts: ScavengerHunt[] = SCAVENGERHUNTS;
 
-  constructor() {}
-
+  constructor() { }
 }
