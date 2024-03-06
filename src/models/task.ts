bf6f4584390022579a6f604872088e27928f4057
Justin <justin-urbanek@bluewin.ch>
2024-03-06 14:47:34 +0100

    style: format tasks

--- src/models/task.ts
+++ src/models/task.ts
@@ -1,15 +1,13 @@
-
-
-export class Task{
+export class Task {
   id: number;
   name: string;
-  timeStart? : Date;
+  timeStart?: Date;
   timeEnd?: Date;
   steaks: number;
   potatoes: number;
   isCompleted: boolean;
 
-  constructor(id:number, name: string) {
+  constructor(id: number, name: string) {
     this.id = id;
     this.name = name;
     this.steaks = 0;
@@ -17,7 +15,7 @@ export class Task{
     this.isCompleted = false;
   }
 
-  completeTask(start: Date, end: Date, steaks: number, potatoes: number){
+  completeTask(start: Date, end: Date, steaks: number, potatoes: number) {
     this.timeStart = start;
     this.timeEnd = end;
     this.steaks = steaks;
