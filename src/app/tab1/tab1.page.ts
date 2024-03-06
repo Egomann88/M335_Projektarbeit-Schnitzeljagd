30a31f2b82e641643a2a1a031bb2ffba80d8abb9
Justin <justin-urbanek@bluewin.ch>
2024-03-06 08:42:11 +0100

    feat: home tab present alert when starting new hunt

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ScavengerHunt } from 'src/models/ScavengerHunt';
+import { User } from 'src/models/User';
+import { UserHelper } from 'src/Helper/UserHelper';
+import { AlertService } from 'src/Services/AlertService';
 import { SCAVENGERHUNTS } from 'src/mocks/ScavengerHuntMock';
 
 @Component({
@@ -9,6 +12,62 @@ import { SCAVENGERHUNTS } from 'src/mocks/ScavengerHuntMock';
 })
 export class Tab1Page {
   scavengerHunts: ScavengerHunt[] = SCAVENGERHUNTS;
+  user: User = new User('', '');
 
-  constructor() { }
+  constructor(private alertService: AlertService) { }
+
+  async startNewScavengerHunt() {
+    const userValid = await this.validateUser();
+    if (!userValid) {
+      console.log("User not valid");
+      return;
+    }
+
+    // start new scavenger hunt
+    console.log("User is valid");
+  }
+
+  async validateUser() {
+    return new Promise((resolve) => {
+      const handler = (data: any) => {
+        this.user = new User(data[0], data[1]);
+        if (UserHelper.getUserName(this.user).trim() === '') {
+          this.alertService.openErrorAlert("Kein gültiger Name eingegeben");
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      };
+
+      this.alertService.PresentAlert(
+        'Neue Schnitzeljagd beginnen',
+        [
+          {
+            text: 'Abbrechen',
+            role: 'cancel',
+            handler: () => resolve(false),  // skips handler and resolves with false
+          },
+          {
+            text: 'Starten',
+            handler,
+          },
+        ],
+        'Bitte gib deinen Namen ein',
+        [
+          {
+            placeholder: 'Vorname',
+            attributes: {
+              minlength: 1,
+            },
+          },
+          {
+            placeholder: 'Nachname',
+            attributes: {
+              minlength: 1,
+            },
+          },
+        ]
+      );
+    });
+  }
 }
