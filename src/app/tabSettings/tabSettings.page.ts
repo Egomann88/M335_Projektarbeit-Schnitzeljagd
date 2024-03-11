b615d96932a1870d5e8c1b27befbcf4df7db5f67
Justin <justin-urbanek@bluewin.ch>
2024-03-11 08:42:12 +0100

    refactor: better design for dev btns

--- src/app/tabSettings/tabSettings.page.ts
+++ src/app/tabSettings/tabSettings.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { UserService } from "src/services/user.service";
 import { User } from 'src/models/User';
+import { ScavengerHunt } from 'src/models/ScavengerHunt';
+import { ScavengerHuntService } from 'src/services/scavenger-hunt-service.service';
 
 @Component({
   selector: 'app-tabSettings',
@@ -10,7 +12,7 @@ import { User } from 'src/models/User';
 export class TabSettingsPage {
   user: User;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private scavengerHuntService: ScavengerHuntService) {
     this.user = new User('', '');
   }
 
@@ -29,5 +31,12 @@ export class TabSettingsPage {
   async resetUser() {
     this.user = new User('', '');
     await this.userService.setUser(this.user);
+  }
+
+  DevInitializeTask(taskIndex: number) {
+    this.scavengerHuntService.currentScavengerHunt = new ScavengerHunt(new Date(), 0, 0, new User('The almighty', 'God'));
+    this.scavengerHuntService.currentIndex = taskIndex;
+    this.scavengerHuntService.currentTask = this.scavengerHuntService.currentScavengerHunt.tasks[taskIndex];
+    this.scavengerHuntService.navigateToNextTask();
   }
 }
