8440de8505551f9e081ce58849a0faed99e70372
dominictosku <dominic.tosku@gmail.com>
2024-03-11 09:15:50 +0100

    refactor: geolocation service

--- src/app/geoloaction/geoloaction.page.ts
+++ src/app/geoloaction/geoloaction.page.ts
@@ -2,6 +2,7 @@ import { Component, NgZone, OnInit } from '@angular/core';
 import { Geolocation } from '@capacitor/geolocation';
 import { Task } from 'src/models/task';
 import { ScavengerHuntService } from 'src/services/scavenger-hunt-service.service';
+import { GeolocationServiceService } from 'src/services/geolocation-service.service'
 
 @Component({
   selector: 'app-geoloaction',
@@ -10,50 +11,20 @@ import { ScavengerHuntService } from 'src/services/scavenger-hunt-service.servic
 })
 export class GeoloactionPage implements OnInit {
   task?: Task;
-  latitude: number = 0
-  longitude: number = 0
 
-  watchId: string = "";
-
-  constructor(private zone: NgZone, private scavengerHuntService: ScavengerHuntService) { }
-
-  options = {
-    enableHighAccuracy: true,
-    timeout: 8000,
-    maximumAge: 7000,
-  };
-
+  constructor(
+    private zone: NgZone,
+    private scavengerHuntService: ScavengerHuntService,
+    public geolocationService: GeolocationServiceService
+  ) { }
 
   async ngOnInit() {
     this.task = this.scavengerHuntService.currentTask;
 
-    await this.watchPosition()
+    await this.geolocationService.watchPosition()
   }
 
-  async getCurrentPosition() {
-    const coordinates = await Geolocation.getCurrentPosition();
-
-    this.latitude = coordinates.coords.latitude;
-    this.longitude = coordinates.coords.longitude;
-  };
-
-  async watchPosition() {
-    this.watchId = await Geolocation.watchPosition(this.options, (position, err) => {
-      if (err) {
-        console.error('Error watching position:', err);
-      } else {
-        this.zone.run(() => {
-          if (position) {
-            this.longitude = position.coords.longitude;
-            this.latitude = position.coords.latitude;
-          }
-        })
-      }
-    });
-  }
-
-  // If you want to stop watching the position
-  stopWatching() {
-    Geolocation.clearWatch({ id: this.watchId });
+  async getCurrentPosition(){
+    await this.geolocationService.getCurrentPosition()
   }
 }
