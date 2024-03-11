6f7151cedd6dd1092921e5c47226cdb2b1f4d887
Justin <justin-urbanek@bluewin.ch>
2024-03-11 11:21:14 +0100

    feat: try-catch to api service

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AlertService } from './AlertService';
 
 @Injectable({
   providedIn: 'root'
@@ -8,19 +9,24 @@ export class ApiService {
   url = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSc9v68rbCckYwcIekRLOaVZ0Qdm3eeh1xCEkgpn3d7pParfLQ/formResponse';
   headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private alertService: AlertService) { }
 
   post(name: string, schnitzel: string, potato: string, hours: number, minutes: number, seconds: number, url?: string, headers?: HttpHeaders) {
-    if (url === undefined) url = this.url;
-    if (headers === undefined) headers = this.headers;
+    try {
+      if (url === undefined) url = this.url;
+      if (headers === undefined) headers = this.headers;
 
-    const formData = new FormData();
+      const formData = new FormData();
 
-    formData.append('entry.1860183935', name);
-    formData.append('entry.564282981', schnitzel);
-    formData.append('entry.1079317865', potato);
-    formData.append('entry.985590604', `${hours}:${minutes}:${seconds} `);
+      formData.append('entry.1860183935', name);
+      formData.append('entry.564282981', schnitzel);
+      formData.append('entry.1079317865', potato);
+      formData.append('entry.985590604', `${hours}:${minutes}:${seconds} `);
 
-    return this.http.post(url, formData, { headers });
+      this.http.post(url, formData, { headers });
+    } catch (e) {
+      console.error(e);
+      this.alertService.openErrorAlert("Beim Senden an das Backend (Google Tabelle) ist ein Fehler aufgetreten.");
+    }
   }
 }
