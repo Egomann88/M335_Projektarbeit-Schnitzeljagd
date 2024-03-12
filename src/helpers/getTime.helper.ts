565f30033f0fee9bcf24765a90989607a5b1832e
Justin <justin-urbanek@bluewin.ch>
2024-03-12 13:57:06 +0100

    refactor: remove unussed code

--- src/helpers/getTime.helper.ts
+++ src/helpers/getTime.helper.ts
@@ -1,6 +1,3 @@
-import { ScavengerHunt } from 'src/models/ScavengerHunt';
-import { Task } from 'src/models/task';
-
 export class GetTimeHelper {
   static getTime(task: any): string {
     if (task.seconds >= 60) return Math.floor(task.seconds / 60) + ' min ' + (task.seconds % 60) + ' s';
